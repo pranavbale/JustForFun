4bde454e95f63f9aee6bd8e12903b3f2cf442be9
Pranav Bale <pranavbale99@gmail.com>
2024-12-04 10:55:39 +0530

    first Commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Main {
+
+    @Test
+    public void runProgram() {
+        System.out.println("Hello world");
+
+        Assert.assertTrue(true);
+    }
+}
